62f9fdf010e1dcaff9957e0fc7536b8941affec9
Alex Schoof <alex@luminal.io>
2016-04-05 09:38:04 -0400

    first commit. listSecrets mostly works and encrypt kind of works

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojstash
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/clojstash/core.clj
@@ -0,0 +1,63 @@
+(ns clojstash.core
+  (:gen-class)
+  (:use [amazonica.aws.dynamodbv2]))
+
+;;(import javax.crypto.Cipher)
+;;(import org.bouncycastle.jce.provider.BouncyCastleProvider)
+;;(import java.security.Security)
+
+(import '(org.bouncycastle.crypto.params KeyParameter))
+(import '(org.bouncycastle.crypto.modes SICBlockCipher))
+(import '(org.bouncycastle.crypto.engines AESEngine))
+(require '[base64-clj.core :as base64])
+
+(defn getSicCipher
+  "Return an AES cipher in SIC (CTR) mode. Configure using key, and set toEncrypt"
+  [key toEncrypt?]
+  ;; TODO: make sure key is 32 bytes long
+  (let [engine (new AESEngine)]
+    (. engine init toEncrypt? (new KeyParameter (.getBytes key)))
+    (new SICBlockCipher engine)))
+
+(defn encrypt
+  "Encrypt plaintext with key using AWS in SIC (CTR) mode. Returns B64-encoded ciphertext string"
+  [plaintext key]
+  (let [cipher (getSicCipher key true)
+        plainBytes (.getBytes plaintext)
+        plainLen (count plainBytes)
+        cipherBytes (byte-array plainLen)]
+    (. cipher processBytes plainBytes 0 plainLen cipherBytes 0)
+    (apply str (map char (base64/encode-bytes cipherBytes)))))
+
+(defn listSecrets
+  "Return a list of secrets and their version in the credential store"
+  ;; Right now region gets ignored
+  [& {:keys [region table]
+      :or {region "us-east-1"
+           table "credential-store"}}]
+  (:items
+   (scan
+    :table-name table
+    :attributes-to-get '["name" "version"])))
+
+(defn printSecrets
+  "Pretty-print secrets and versions"
+  [& {:keys [region table]
+      :or {region "us-east-1"
+           table "credential-store"}}]
+  (println
+   (clojure.string/join "\n"
+    (map
+     (fn [x]
+       (format "%s -- version %s" (:name x) (:version x)))
+     (listSecrets region table)))))
+
+
+
+
+(defn -main
+  "I don't do a whole lot."
+  []
+  ; Register Bouncy Castle as a crypto provider
+;  (. Security addProvider (new BouncyCastleProvider))
+  (printSecrets :region "us-east-1" :table "credential-store"))
